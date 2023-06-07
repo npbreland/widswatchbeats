7f6fad1b175cec445f1d1379037674fd8205c19c
Nick Breland <npbreland@tutanota.com>
2023-06-07 11:38:36 +0100

    Initial commit with code

---
+++ widswatchbeats.wid.js
@@ -0,0 +1,41 @@
+(() => {
+  const WIDTH = 50;
+  const SEC_PER_BEAT = 86.4;
+
+  let drawTimeout;
+
+  function getSecondsSinceMidnight() {
+    const now = new Date();
+    return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+  }
+
+  function queueDraw() {
+    if (drawTimeout) clearTimeout(drawTimeout);
+    const nextSecond = SEC_PER_BEAT - (getSecondsSinceMidnight() % SEC_PER_BEAT);
+    drawTimeout = setTimeout(function() {
+      drawTimeout = undefined;
+      draw();
+    }, nextSecond * 1000);
+  }
+
+  function draw() {
+    const now = new Date();
+    const seconds = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+    const beats = Math.floor(seconds / SEC_PER_BEAT);
+    const beatsString = '@' + beats.toString().padStart(3, '0');
+
+    g.reset();
+    g.setFontAlign(0, 0);
+    g.setFont("6x8", 2);
+    g.drawString(beatsString, this.x+WIDTH/2, this.y+12);
+    queueDraw();
+  }
+
+  WIDGETS.widswatchbeats = {
+    area: "tl",
+    width: WIDTH,
+    draw
+  };
+
+  draw();
+})();
