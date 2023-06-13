2893b31555262778414284b86da945e6900c66fb
Nick Breland <npbreland@tutanota.com>
2023-06-13 21:10:46 +0100

    Fix display bugs; add ms to ensure after beat

--- widswatchbeats.wid.js
+++ widswatchbeats.wid.js
@@ -1,7 +1,7 @@
-(() => {
+(function() {
   const WIDTH = 50;
   const SEC_PER_BEAT = 86.4;
-
+  
   let drawTimeout;
 
   function getSecondsSinceMidnight() {
@@ -14,8 +14,8 @@
     const nextSecond = SEC_PER_BEAT - (getSecondsSinceMidnight() % SEC_PER_BEAT);
     drawTimeout = setTimeout(function() {
       drawTimeout = undefined;
-      draw();
-    }, nextSecond * 1000);
+      WIDGETS.widswatchbeats.draw();
+    }, nextSecond * 1000 + 1); // Add one ms to ensure we're past the beat
   }
 
   function draw() {
@@ -23,9 +23,10 @@
     const seconds = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
     const beats = Math.floor(seconds / SEC_PER_BEAT);
     const beatsString = '@' + beats.toString().padStart(3, '0');
-
+    
     g.reset();
     g.setFontAlign(0, 0);
+    g.clearRect(this.x, this.y, this.x + WIDTH, this.y+22);
     g.setFont("6x8", 2);
     g.drawString(beatsString, this.x+WIDTH/2, this.y+12);
     queueDraw();
